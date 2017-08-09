c0c5e110c2880d0b8fdb13374efd0914c533ccb5
himanshu pandey <himanshu.pandey7070@yahoo.com>
2017-08-09 14:05:43 +0530

    Add files via upload
    updated news app

--- himanshu/app.js
+++ himanshu/app.js
@@ -1,11 +1,11 @@
  var $selectnews  = $('#news');
  $.getJSON('http://192.168.1.5:5000/news/',function(data){
-
   $selectnews.html('');
 
 for (var n = 0; n< data['data'].length; n++) {
 
  $selectnews.append('<li id ="'+data['data'][n]['id'] +'" >' + data['data'][n]['title'] + '</li>' );
+ $selectnews.append('<li> <img src id ="'+data['data'][n]['image_url'] +'" >' + data['data'][n]['image_url'] + '</li>' );
 
   }
 
@@ -39,22 +39,22 @@ for (var j = 0; j< data['data'].length; j++) {
 
  $selectcity.append('<option value ="'+data['data'][j]['id'] +'" >' + data['data'][j]['city'] + '</option>' );
 
-// $.each(data,function(key,value){
+
 }
 });
 
 $selectcity.change(function(){
 
-var cityid  = $( "#city" ).index();
+var cityid  = $( "#city :selected" ).index();
 
 
 $.ajax({
 
  	type:"GET",
-    url: "http://192.168.1.5:5000/news",
-    data:  "id="+cityid,
+    url: "http://192.168.1.5:5000/news/cities/"+cityid+"/" +"languages/"+1 ,
+    data: {},
     success: function(data){
-   
+   console.log(data)
 
 var $selectnews  = $('#news');
  
